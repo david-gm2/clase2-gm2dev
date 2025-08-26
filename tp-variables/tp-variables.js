5f8132d267568555dc93d66bd7026682390ad681
david.alvarez <david.alvarez@gm2dev.com>
2025-08-26 11:26:23 -0300

    docs(tp-variables.js): add project instructions as comments

---
+++ tp-variables/tp-variables.js
@@ -0,0 +1,45 @@
+// TODO: TP VARIABLES JS
+
+// * Declarar una variable de tipo string cuyo nombre represente de forma clara su contenido.
+
+// * Declarar una variable de tipo number cuyo nombre represente de forma clara su contenido.
+
+// * Declarar una variable de tipo boolean cuyo nombre represente de forma clara su contenido.
+
+// * Declarar dos variables de tipo string. Luego declarar una nueva variable cuyo valor sea la concatenación de las dos variables previamente creadas.
+
+// * Declarar dos variables de tipo number.
+//  Luego declarar una nueva variable cuyo valor sea la suma de las dos variables previamente creadas.
+//  Luego declarar una nueva variable cuyo valor sea la resta de las dos variables previamente creadas.
+//  Luego declarar una nueva variable cuyo valor sea la multiplicación de las dos variables previamente creadas.
+//  Luego declarar una nueva variable cuyo valor sea la división de las dos variables previamente creadas.
+//  Luego declarar una nueva variable cuyo valor sea el módulo entre las dos variables previamente creadas.
+
+// * Declarar una variable de tipo number y otra de tipo string. Luego sumar ambas variables y guardar su resultado en una nueva variable. De qué tipo será la tercera variable?
+
+// * Declarar una variable de tipo booleano cuyo nombre represente su valor. Luego declarar una nueva variable cuyo valor sea opuesto a la variable previamente creada.
+
+// * Crear una variable de tipo number.
+//  Reasignar un nuevo valor a esa misma variable para que sea su valor + 1.
+//  Repetir la operación anterior utilizando una estrategia diferente
+//  Repetir ambas operaciones con operadores de resta, multiplicación y división.
+
+// * Declarar una constante e intentar reasignarle otro valor luego de su declaración. Que output recibis?
+
+// * Crear dos variables de tipo string y dos variables de tipo number.
+//  Crear una nueva variable cuyo valor sea un string que incluya las cuatro variables previamente creadas.
+//  Repetir el punto anterior utilizando una estrategia diferente.
+
+// * Crear dos variables de tipo number.
+//  Crear cuatro nuevas variables de tipo boolean comparando los dos valores utilizando operadores de comparación.
+
+// * Declarar cinco variables de tipo string cuyo valor contenga operaciones aritméticas. Intenta utilizar diferentes estrategias.
+
+// * Crea un bloque de código que, utilizando variables con nombres representativos, calcule la cantidad de propina a dejar en un restaurante, basada en el total de la cuenta y el porcentaje de propina ingresado por el usuario.
+let totalCuenta = 1200;
+
+// * Crea un bloque de código que calcule el precio final de un producto luego de aplicar un descuento.
+
+// * Crea un bloque de código que, tomando el año de nacimiento de una persona, devuelva su edad actual.
+
+// * Crea un bloque de código que, definiendo las variables necesarias, calcule el área de un círculo, un cuadrado y un triángulo.
