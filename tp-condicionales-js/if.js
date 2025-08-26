ba919ea565d845585ad0a7a5f685869cac922326
david.alvarez <david.alvarez@gm2dev.com>
2025-08-26 11:45:03 -0300

    docs(tp-condicionales-js/if.js): add project instructions as comments

---
+++ tp-condicionales-js/if.js
@@ -0,0 +1,17 @@
+// TP CONDICIONALES JS
+
+// TODO: If:
+
+// * Crea un bloque de código que compare dos variables numéricas. Si el número A es mayor que el número B, imprimir en consola ‘El número A es mayor a B‘.
+
+// * Crea un bloque de código que tome la edad de un usuario y la compare con un número de referencia. Si la edad es mayor al número de referencia, imprimir en consola un mensaje.
+
+// * Crea un bloque de código que compare dos números e imprima en consola un valor booleano.
+
+// * Crea un bloque de código que compare dos nombres, y si ambos son iguales imprima en consola el nombre.
+
+// * Crea un bloque de código que compare dos nombres, y si ambos son diferentes imprima en consola un mensaje.
+
+// * Crea un bloque de código que imprima en consola un número en caso de que sea un número negativo.
+
+// * Crea un bloque de código que imprima en consola un número en caso de que NO sea un número negativo.
