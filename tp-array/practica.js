74f039e1a8d695e90fcca813568653542d62f384
david.alvarez <david.alvarez@gm2dev.com>
2025-08-28 09:51:02 -0300

    feat(tp-array/practica.js): update implementation with optimized solution

--- tp-array/practica.js
+++ tp-array/practica.js
@@ -1,15 +1,27 @@
-let number = {
-  nums: [2, 4, 5, 7, 8],
-  target: 6,
+let exercises = {
+  example1: {
+    nums: [2, 7, 11, 15],
+    target: 9,
+  },
+  example2: {
+    nums: [3, 2, 4],
+    target: 6,
+  },
+  example3: {
+    nums: [3, 3],
+    target: 6,
+  },
 };
 
 function encontrarSuma(number) {
   for (let i = 0; i < number.nums.length - 1; i++) {
     let suma = number.nums[i] + number.nums[i + 1];
     if (suma === number.target) {
-      console.log(number.nums[i], number.nums[i + 1]);
+      console.log([number.nums[i], number.nums[i + 1]]);
     }
   }
 }
 
-encontrarSuma(number);
+encontrarSuma(exercises.example1);
+encontrarSuma(exercises.example2);
+encontrarSuma(exercises.example3);
