e81c98950c0a1c8e2f60b676e723c11ec2922d6f
david.alvarez <david.alvarez@gm2dev.com>
2025-09-01 13:03:10 -0300

    feat(tp-loop/tp-loops.js): complete exercise 1

--- tp-loop/tp-loops.js
+++ tp-loop/tp-loops.js
@@ -1,5 +1,14 @@
 // ** Loops
 // TODO: 1. Usa el bucle for para mostrar números pares del 2 al 30.
+
+function exercise1() {
+  for (let i = 2; i <= 30; i++) {
+    console.log(i);
+  }
+}
+
+// exercise1();
+
 // TODO: 2. Escribe un bucle que solicite un número mayor que 250. Si el usuario ingresa otro número debes pedirle que ingrese un valor nuevo El bucle debe pedir un número hasta que el usuario ingrese un número mayor que 250 o lo cancele Aquí podemos asumir que el usuario solo ingresará números.
 // TODO: 3. Un número entero mayor que 1 es llamado primo si no puede ser dividido sin un resto por ningún número excepto 1 y él mismo. En otras palabras, n > 1 es un primo si no puede ser dividido exactamente por ningún número excepto 1 y n. Por ejemplo, 5 es un primo, porque no puede ser dividido exactamente por 2, 3 y 4. Escribe el código que muestre números primos en el intervalo de 2 a n. Ejemplo: Para n = 10 el resultado será 2, 3, 5, 7.
 // TODO: 4. Ingrese un número y mostrar su potencia n^2, repetir este proceso hasta que se ingrese un número negativo
