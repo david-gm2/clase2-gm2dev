bc30a6b3947a49367f47ffd734ed141211b20953
david.alvarez <david.alvarez@gm2dev.com>
2025-09-03 12:56:19 -0300

    feat(tp-objects/objects-dia2.js): complete exercise 3

--- tp-objects/objects-dia2.js
+++ tp-objects/objects-dia2.js
@@ -12,3 +12,8 @@ const burro = { nombre: "Burro", edad: 25, especie: "Burro parlante" };
 const { nombre: nombrePersonaje, edad: edadPersonaje } = burro;
 console.log(nombrePersonaje, edadPersonaje);
 
+// TODO 3: Objeto Fiona con valor por defecto
+const fiona = { nombre: "Fiona", especie: "Ogro" };
+const { nombre: nombreFiona, ocupacion = "desconocida" } = fiona;
+console.log(nombreFiona, ocupacion); 
+
