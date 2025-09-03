158ce87345896c86a746aad332f809dc021c1332
david.alvarez <david.alvarez@gm2dev.com>
2025-09-03 11:45:22 -0300

    fix(index.js): reformat

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
-const prompt = require("prompt-sync")();
+const prompt = require('prompt-sync')()
 
 class BaseProducto {
-  constructor({ nombre, precio, descripcion }) {
-    this.nombre = nombre;
-    this.precio = precio;
-    this.descripcion = descripcion;
+  constructor ({ nombre, precio, descripcion }) {
+    this.nombre = nombre
+    this.precio = precio
+    this.descripcion = descripcion
   }
 }
 
-let pan = new BaseProducto({
-  descripcion: "pan lactal",
-  nombre: "pan",
-  precio: 123,
-});
+const pan = new BaseProducto({
+  descripcion: 'pan lactal',
+  nombre: 'pan',
+  precio: 123
+})
 
-console.log(pan);
+console.log(pan)
