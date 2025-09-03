2a7e223ba657e75cac0b14500ff937b4729867f3
david.alvarez <david.alvarez@gm2dev.com>
2025-09-03 12:56:03 -0300

    feat(tp-objects/objects-dia2.js): complete exercise 1

---
+++ tp-objects/objects-dia2.js
@@ -0,0 +1,8 @@
+// =============================
+// PARTE 1: Destructuring Básico
+// =============================
+
+// TODO 1: Crear objeto Shrek y extraer propiedades
+const shrek = { nombre: "Shrek", especie: "Ogro" };
+const { nombre, especie } = shrek;
+console.log(nombre, especie);
