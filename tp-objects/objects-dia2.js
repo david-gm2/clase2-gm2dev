5fe1ce8cdc4f7def506cfe7666433a8d01f310f5
david.alvarez <david.alvarez@gm2dev.com>
2025-09-03 12:56:12 -0300

    feat(tp-objects/objects-dia2.js): complete exercise 2

--- tp-objects/objects-dia2.js
+++ tp-objects/objects-dia2.js
@@ -6,3 +6,9 @@
 const shrek = { nombre: "Shrek", especie: "Ogro" };
 const { nombre, especie } = shrek;
 console.log(nombre, especie);
+
+// TODO 2: Objeto Burro con alias en destructuring
+const burro = { nombre: "Burro", edad: 25, especie: "Burro parlante" };
+const { nombre: nombrePersonaje, edad: edadPersonaje } = burro;
+console.log(nombrePersonaje, edadPersonaje);
+
